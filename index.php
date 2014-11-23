4f54e8cd33769f264e03d0210f21902c9446bb83
Anou Simmavong <anou.sim@gmail.com>
2014-11-23 19:52:32 +0100

    Revert "test"
    This reverts commit 3cd4d291d08b452cae38e94515bf82561dd4671e.

--- index.php
+++ index.php
@@ -3,7 +3,6 @@
 <head>
 	<title>Chicha Shop</title>
 	<link rel="stylesheet" href="css/main.css">
-	<link href='http://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
 	<script src="js/modernizr.min.js" type="text/javascript"></script>
 	<script type="text/javascript" src="//localhost:35729/livereload.js"></script>
 </head>
@@ -32,13 +31,7 @@
 		</div>
 	</div>
 	<main>
-		<h1>titre 1</h1>
-		<h1>titre 2</h1>
-		<h1>kokdaokoa</h1>
-		<div></div>
-		<div></div>
-		<div></div>
-		<div></div>
+		
 	</main>
 	<footer>
 		<div>
